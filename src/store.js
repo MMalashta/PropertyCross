c76ae994240f1eb57a0f1170ea3cd2ffb855d768
MMalashta <maxim.malashta@gmail.com>
2017-02-09 21:32:17 +0200

    Add redux-devtools

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import { routerMiddleware as createRouterMiddleware } from 'react-router-redux'
 import { browserHistory } from 'react-router'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducers'
 
 const routerMiddleware = createRouterMiddleware(browserHistory)
 
-export default createStore(reducer, applyMiddleware(routerMiddleware))
+export default createStore(reducer, composeWithDevTools(applyMiddleware(routerMiddleware)));
